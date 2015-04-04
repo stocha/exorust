e68ad1d88bb950b5ef29bb5c823b7c1c809f81ce
DFI <dfi@dfidaali.com>
2015-04-04 17:18:40 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+
+
+fn print(x : Vec<i64>){
+	println!("x is: {:?}", x);
+}
+
 fn main() {
 
 	let xs: [i64; 6] = [0, 0, 0, 0, 0, 0];
 
-
+	print("{:?}", xs.iter.collect);
     println!("Hello, world!");
 }
