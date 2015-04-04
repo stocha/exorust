35290dd07c8fae662e32da4a8f0bb6a4b355cdd8
DFI <dfi@dfidaali.com>
2015-04-04 18:04:40 +0200

    helloWorld

---
+++ src/exo/hello_world/mod.rs
@@ -0,0 +1,5 @@
+
+pub fn launch()->(){
+	println!("Hello, world!");
+	
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 
+mod common;
+mod exo;
 
-fn print(x : Vec<i64>){
-	println!("x is: {:?}", x);
-}
+
 
 fn main() {
-
-	let xs: [i64; 6] = [0, 0, 0, 0, 0, 0];
-
-	print("{:?}", xs.iter.collect);
-    println!("Hello, world!");
+	exo::hello_world::launch();
+    
 }
